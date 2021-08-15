86117ef3d19dc18fde6fceba9b996da9d5b4f6f7
Azi <you@example.com>
2021-08-15 17:39:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [randomColor, setrandomColor] = useState()
+  const [range, setRange] = useState(100)
+
+  const changeColor = () => {
+     setrandomColor('#'+Math.floor(Math.random()*16777215).toString(16))    
+  }
+  const changeRange = (e) => {
+     setRange(e.target.value)
+  }
+
+
+   let numbers = []
+   let i = 1
+    for (i = 1 ; i <= range ; i++){
+      numbers[i] =+ i
+    }
+    
+    return (
+      <div className="App"> 
+        <div className="table">
+             <div className="head">
+                <h2>This is a list of numbers from 1 to 100, every number divisble by 3 
+                      turns into the word "Robot", the ones divisble by 5 turn into the word "ICT" and the ones
+                      divisible by both 3 & 5 are turned into the word "Robot ICT"  
+                </h2>
+                <input onChange={changeRange} placeholder="Set your range"></input>
+                <button onClick={changeColor}>Click to change color</button>
+             </div>
+          <ul className="body">
+              {numbers.map((inx, index) => {
+                if (inx % 3 === 0 && inx % 5 === 0 ){
+                  return <li style ={{backgroundColor: randomColor}} key={index}>RobotICT</li>
+                }
+                else if (inx % 5 === 0 ){
+                  return <li style ={{backgroundColor: randomColor}} key={index}>ICT</li>
+                }
+                else if (inx % 3 === 0 ){
+                  return <li style ={{backgroundColor: randomColor}} key={index}>Robot</li>
+                }
+                else return <li key={index}>{inx}</li>
+                }   
+              )}
+          </ul>
+       </div>
+      </div>
+    );  
+  
 }
 
 export default App;
